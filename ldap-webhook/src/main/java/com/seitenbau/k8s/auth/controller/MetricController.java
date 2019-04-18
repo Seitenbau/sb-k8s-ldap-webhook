4604d882d76dc3605581597ec42b434cb1236e9c
Jakob Dörsing <jakob.doersing@seitenbau.com>
2019-04-18 11:44:58 +0200

    add prometheus metrics

---
+++ ldap-webhook/src/main/java/com/seitenbau/k8s/auth/controller/MetricController.java
@@ -0,0 +1,37 @@
+package com.seitenbau.k8s.auth.controller;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+
+@RestController
+@Slf4j
+public class MetricController {
+
+    private MeterRegistry meterRegistry;
+
+    @Inject
+    public MetricController(MeterRegistry meterRegistry)
+    {
+        this.meterRegistry = meterRegistry;
+    }
+
+
+    @RequestMapping(value="/metrics", produces = TEXT_PLAIN_VALUE)
+    public String metrics() {
+
+        meterRegistry.counter("http.requests", "path", "/metrics", "code", Integer.toString(HttpStatus.OK.value())).increment();
+        return ((PrometheusMeterRegistry) meterRegistry).scrape();
+
+    }
+
+
+}
